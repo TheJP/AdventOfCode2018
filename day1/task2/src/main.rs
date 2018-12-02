c7af6f53175f65a0681af1bbf077d95016cc97bb
TheJP <jp_55555@hotmail.com>
2018-12-02 12:03:49 +0100

    Fixed task order for day 1

--- day1/task2/src/main.rs
+++ day1/task2/src/main.rs
@@ -2,24 +2,38 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::io;
+use std::collections::HashSet;
+use std::io::Seek;
+use std::io::SeekFrom;
 
 fn main() -> io::Result<()> {
-    let f = File::open("../input1.txt")?;
+    let f = File::open("../input2.txt")?;
     let mut reader = BufReader::new(f);
     let mut buffer = String::new();
     let mut frequency: i32 = 0;
+    let mut frequencies = HashSet::new();
+    frequencies.insert(0);
 
-    while reader.read_line(&mut buffer)? > 0 {
-        let number = buffer.trim().parse::<i32>();
-        if let Ok(number) = number {
-            frequency += number;
-        } else if let Err(e) = number {
-            println!("Error: {}, '{}'", e, buffer);
+    loop {
+        while reader.read_line(&mut buffer)? > 0 {
+            let number = buffer.trim().parse::<i32>();
+            if let Ok(number) = number {
+                frequency += number;
+            } else if let Err(e) = number {
+                println!("Error: {}, '{}'", e, buffer);
+            }
+
+            if frequencies.contains(&frequency) {
+                println!("Result {}", frequency);
+                return Ok(());
+            } else {
+                frequencies.insert(frequency);
+                println!("{}", frequency);
+            }
+
+            buffer = String::new();
         }
-        buffer = String::new();
+
+        reader.seek(SeekFrom::Start(0))?;
     }
-
-    println!("Result {}", frequency);
-
-    Ok(())
 }
