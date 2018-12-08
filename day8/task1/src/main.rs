e9a942ea65fba6dbdef4aadf67999cf5b455ecd8
TheJP <jp_55555@hotmail.com>
2018-12-08 09:12:23 +0100

    Solved day 8 part 2

--- day8/task1/src/main.rs
+++ day8/task1/src/main.rs
@@ -7,6 +7,9 @@ use std::slice::Iter;
 struct Node {
     children: Vec<Box<Node>>,
     metadata: Vec<i32>,
+
+    // Part 2
+    data: i32,
 }
 
 fn construct_tree(numbers: &mut Iter<i32>, current: &mut Box<Node>) -> i32 {
@@ -14,8 +17,8 @@ fn construct_tree(numbers: &mut Iter<i32>, current: &mut Box<Node>) -> i32 {
     let size = *numbers.next().unwrap();
     let mut sum = 0;
 
-        for _ in 0..children {
-        let mut child = Box::new(Node { children: Vec::new(), metadata: Vec::new() });
+    for _ in 0..children {
+        let mut child = Box::new(Node { children: Vec::new(), metadata: Vec::new(), data: 0 });
         sum += construct_tree(numbers, &mut child);
         current.children.push(child);
     }
@@ -24,6 +27,15 @@ fn construct_tree(numbers: &mut Iter<i32>, current: &mut Box<Node>) -> i32 {
         let data = *numbers.next().unwrap();
         current.metadata.push(data);
         sum += data;
+    }
+
+    // Part 2
+    if children <= 0 {
+        current.data = sum;
+    } else {
+        current.data = current.metadata.iter()
+            .filter_map(|&index| current.children.get(index as usize - 1))
+            .map(|node| node.data).sum();
     }
 
     sum
@@ -39,11 +51,12 @@ fn main() -> io::Result<()> {
             .map(|token| token.parse::<i32>().unwrap()).collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    let mut tree = Box::new(Node { children: Vec::new(), metadata: Vec::new() });
+    let mut tree = Box::new(Node { children: Vec::new(), metadata: Vec::new(), data: 0 });
 
     let sum = construct_tree(&mut numbers.iter(), &mut tree);
 
     println!("Sum: {}", sum);
+    println!("Value: {}", tree.data);
 
     Ok(())
 }
