9f533121f2492e56519bb35fa483dcaf6b408d1d
TheJP <jp_55555@hotmail.com>
2018-12-19 23:27:43 +0100

    Started to solve day 11 part 1

---
+++ day11/task1/src/main.rs
@@ -0,0 +1,45 @@
+const SERIAL: i64 = 7165;
+const DIM: usize = 300;
+
+fn power_level(x: i64, y: i64, serial: i64) -> i64 {
+    let rack_id = x + 10;
+    let power_level = (rack_id * y + serial) * rack_id;
+    return (power_level / 100) % 10 - 5;
+}
+
+fn find_best(serial: i64) -> (usize, usize) {
+    let mut grid = vec![vec![0; DIM]; DIM];
+    for y in 0..DIM {
+        for x in 0..DIM {
+            grid[y][x] = power_level(x as i64, y as i64, serial);
+        }
+    }
+
+    (0..DIM).flat_map(|y| (0..DIM).map(|x| (y, x)));
+    unimplemented!();
+}
+
+fn main() {
+    let (x, y) = find_best(SERIAL);
+    println!("{},{}", x, y);
+}
+
+#[cfg(test)]
+mod test {
+    use power_level;
+    use find_best;
+
+    #[test]
+    fn test_power_level() {
+        assert_eq!(4, power_level(3, 5, 8));
+        assert_eq!(-5, power_level(122, 79, 57));
+        assert_eq!(0, power_level(217, 196, 39));
+        assert_eq!(4, power_level(101, 153, 71));
+    }
+
+    #[test]
+    fn test_find_best() {
+        assert_eq!((33, 45), find_best(18));
+        assert_eq!((21, 61), find_best(42));
+    }
+}
